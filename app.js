e437e810c3841b2cbe9d4889e16be0a800eda572
Anuj Kumar <anujkumar21052004@gmail.com>
2026-02-18 16:49:02 +0530

    Added CustomError Handling

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.use(express.json());
 const authorRouter = require("./routes/authorRouter");
 
 app.use("/authors", authorRouter);
+app.use((req, res, next) => {
+ next(new Error("OH NO!"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
