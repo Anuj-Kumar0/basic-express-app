71ac0cf90e2db27b0e2d3a6294a82e5c86ae5e15
Anuj Kumar <anujkumar21052004@gmail.com>
2026-02-18 08:11:41 +0530

    Added Controllers

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,16 +1,19 @@
 const { Router } = require("express");
+const { getAuthorById } = require('../controllers/authorController');
+
 
 const authorRouter = Router();
 
-authorRouter.get("/", (req, res) => res.send("All authors"));
-authorRouter.get("/:authorId", (req, res) => {
-  const { authorId } = req.params;
-  res.send(`Author ID: ${authorId}`);
-});
-authorRouter.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("Author created");
-  });
+authorRouter.get("/:authorId", getAuthorById);
+// authorRouter.get("/", (req, res) => res.send("All authors"));
+// authorRouter.get("/:authorId", (req, res) => {
+//   const { authorId } = req.params;
+//   res.send(`Author ID: ${authorId}`);
+// });
+// authorRouter.post("/", (req, res) => {
+//     console.log(req.body);
+//     res.send("Author created");
+//   });
   
 
 module.exports = authorRouter;
