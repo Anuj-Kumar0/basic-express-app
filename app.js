4f05aa25da0cf8d1e0ee1390865351492faa252a
Anuj Kumar <anujkumar21052004@gmail.com>
2026-02-20 08:43:38 +0530

    Added Views, serve static files

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+
+
+app.use(express.static(assetsPath));
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const aboutLinks = [
+  { href: "/", text: "Home" },
+  { href: "contact", text: "Contact" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("index", { links: links , users: users});
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {aboutLinks: aboutLinks} );
+});
 
 const authorRouter = require("./routes/authorRouter");
 
